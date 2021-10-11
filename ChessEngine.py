52f4a457a47dd465bfdb7dbc16026a2ed161db6f
Nittishna <75472177+Xyandraa9@users.noreply.github.com>
2021-10-11 23:37:34 +0530

    added code for the board GUI

---
+++ ChessEngine.py
@@ -0,0 +1,15 @@
+# storing all information about current state of Chess game.
+# Also responsible for determining valid moves at current stats.
+class GameState():
+    def __init__(self):
+        self.board =[
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bK", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wK", "wR"]]
+        self.whiteToMove = True
+        self.movelog = []
