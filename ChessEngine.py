d847da4e70ba6190ed45da5dd01656ad3374e390
Nittishna <75472177+Xyandraa9@users.noreply.github.com>
2021-10-12 17:54:17 +0530

    added undo moves and made valid moves.

--- ChessEngine.py
+++ ChessEngine.py
@@ -21,6 +21,46 @@ class GameState():
         self.whiteToMove = not self.whiteToMove  # swap players
 
 
+    def undoMove(self):
+        if len(self.moveLog) != 0:  # making sure there is a move to undo
+            move = self.moveLog.pop()
+            self.board[move.startRow][move.startCol] = move.pieceMoved
+            self.board[move.endRow][move.endCol] = move.pieceCaptured
+            self.whiteToMove = not self.whiteToMove  # switch turns back.
+
+    '''
+    all moves considering checks
+    '''
+    def getValidMoves(self):
+        return self.getAllPossibleMoves()
+
+    '''
+    all moves without considering checks
+    '''
+    def getAllPossibleMoves(self):
+        moves = [Move((6, 4), (4, 4), self.board)]
+        for r in range(len(self.board)):
+            for c in range(len(self.board[r])):
+                turn = self.board[r][c][0]
+                if (turn == 'w' and self.whiteToMove) and (turn =='b' and not self.whiteToMove):
+                    piece = self.board[r][c][1]
+                    if piece == 'p':
+                        self.getPawnMoves(r, c, moves)
+                    elif piece == 'R':
+                        self.getRookMoves(r, c, moves)
+        return moves
+    '''
+    Get all the pawn moves for the pawn located at  row, col and  add these moves to the  list
+    '''
+    def getPawnMoves(self, r, c, moves):
+        pass
+
+    '''
+    Get all the rook moves for the rook located at  row, col and  add these moves to the  list
+    '''
+    def getRookMoves(self, r, c, moves):
+        pass
+
 class Move():
 
     # maps keys to values
@@ -39,6 +79,18 @@ class Move():
         self.endCol = endSq[1]
         self.pieceMoved = board[self.startRow][self.startCol]
         self.pieceCaptured = board[self.endRow][self.endCol]
+        self.moveID = self.startRow * 1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
+        print(self.moveID)
+
+    '''
+    Overriding the equals method
+    '''
+
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.moveID == other.moveID
+        return False
+
 
     def getChessNotation(self):
         return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
