48aa9d23e610f855fe16a3716ceb585900783f01
Nittishna <75472177+Xyandraa9@users.noreply.github.com>
2021-10-12 17:55:16 +0530

    added chessmain code for undo and valid moves

--- ChessMain.py
+++ ChessMain.py
@@ -31,6 +31,8 @@ def main():
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     gs = ChessEngine.GameState()
+    validMoves = gs.getValidMoves()
+    moveMade = False  # flag variable for when a move is made
     loadImages()   # only once before while loop
     running = True
     sqSelected = ()  # no square is selected. keep track of last click of user.
@@ -39,6 +41,7 @@ def main():
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            # mouse handler
             elif e.type == p.MOUSEBUTTONDOWN:
                 location = p.mouse.get_pos()  # (x,y) location of mouse.
                 col = location[0] // SQ_SIZE
@@ -52,10 +55,20 @@ def main():
                 if len(playerClicks) == 2:  # after 2nd click
                     move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
                     print(move.getChessNotation())
-                    gs.makeMove(move)
+                    if move in validMoves:
+                        gs.makeMove(move)
+                        moveMade = True
                     sqSelected = ()  # reset user clicks
                     playerClicks = []
+            # key handlers
+            elif e.type == p.KEYDOWN:
+                if e.key == p.K_z: # undo when z is pressed.
+                    gs.undoMove()
+                    moveMade = True
 
+        if moveMade:
+            validMoves = gs.getValidMoves()
+            moveMade = False
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
@@ -84,3 +97,4 @@ def drawPieces(screen, board):
 
 if __name__ == "__main__":
     main()
+
